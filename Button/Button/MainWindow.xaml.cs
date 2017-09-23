936aa3c1fcd4b4fdce9e589399b82753e2c009e7
Elizabeth Nanthavong <enanthav@gmail.com>
2017-09-22 20:20:37 -0600

    Added animations and car button

--- Button/Button/MainWindow.xaml.cs
+++ Button/Button/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Media.Animation;
 
 namespace Button
 {
@@ -20,9 +23,43 @@ namespace Button
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random r = new Random();
+        private DispatcherTimer sleepTimer = new DispatcherTimer();
+        private bool pressed = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnCarlos_MouseMove(object sender, MouseEventArgs e)
+        {
+            //runs everytime the mouse position is updated within the control
+            if (pressed == false)
+            {
+                //mouse button not held down, we do nothing
+                return;
+            }
+
+            Point mousePos = e.GetPosition(this);
+
+            Debug.WriteLine("Please stop bothering me");
+            btnCarlos.Margin = new Thickness(
+                mousePos.X - (btnCarlos.Width / 2),
+                mousePos.Y - (btnCarlos.Height / 2), 0, 0);
+
+        }
+
+        private void btnCarlos_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            pressed = true;
+            btnCarlos.Source = new BitmapImage(new Uri(@"/assets/drive.png", UriKind.Relative));
+        }
+
+        private void btnCarlos_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            pressed = false;
+        }
     }
 }
+
