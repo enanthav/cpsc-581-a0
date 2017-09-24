a63802923a023a8dbe2a4b7d26394c2014d53a7c
Elizabeth Nanthavong <enanthav@gmail.com>
2017-09-24 17:01:00 -0600

    Attempted to fix animations but having errors

--- Button/Button/MainWindow.xaml.cs
+++ Button/Button/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 using System.Windows.Media.Animation;
 using System.Media;
 
@@ -25,9 +24,9 @@ namespace Button
     public partial class MainWindow : Window
     {
         private Random r = new Random();
-        private DispatcherTimer sleepTimer = new DispatcherTimer();
         private bool pressed = false;
         private bool tokyoBackground = false;
+        private bool curryEaten = false;
         private SoundPlayer drivingSound = new SoundPlayer("assets\\driving.wav");
         private SoundPlayer risingSun = new SoundPlayer("assets\\beat of rising sun.wav");
         public MainWindow()
@@ -37,9 +36,23 @@ namespace Button
             drivingSound.Load();
             risingSun.Load();
 
-            // Setup sleep animation timer
-            sleepTimer.Tick += Timer_Tick;
-            sleepTimer.Interval = System.TimeSpan.FromMilliseconds(4000);
+            // Storyboards
+            Storyboard driveSB = this.Resources["DriveInitialD"] as Storyboard;
+            driveSB.Completed += driveSB_Completed;
+
+            Storyboard anticipate = this.Resources["Anticipation"] as Storyboard;
+            anticipate.Completed += anticipate_Completed;
+        }
+
+        private void driveSB_Completed(object sender, EventArgs e)
+        {
+            btnCarlos.Source = new BitmapImage(new Uri(@"/assets/Carlos button.png", UriKind.Relative));
+            btnCarlos.RenderTransform = new TranslateTransform();
+        }
+
+        private void anticipate_Completed(object sender, EventArgs e)
+        {
+            btnCarlos.RenderTransform = new TranslateTransform();
         }
 
         private void btnCarlos_MouseMove(object sender, MouseEventArgs e)
@@ -51,11 +64,12 @@ namespace Button
                 return;
             }
 
-            Storyboard anticipate = this.FindResource("Anticipation") as Storyboard;
             Point mousePos = e.GetPosition(this);
 
-            Debug.WriteLine("Mouse Moving!");
+
             /*
+            Debug.WriteLine("Mouse Moving!");
+            Storyboard anticipate = this.Resources["Anticipation"] as Storyboard;
             // check x, y coordinates to see if they match the x, y coordinates of the elements on the map
             if (((mousePos.X >= 390) & (mousePos.X <= 390 + btnCarlos.Width)) & ((mousePos.Y >= 620) & (mousePos.Y <= 625 + btnCarlos.Height)))
             {
@@ -87,17 +101,25 @@ namespace Button
                 mousePos.Y - (btnCarlos.Height / 2), 0, 0);
         }
         // Event handler for mouse click
-        private void btnCarlos_MouseDown(object sender, MouseButtonEventArgs e)
+        private void btnCarlos_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             pressed = true;
             // Determine if Tokyo background is set
             if (tokyoBackground)
             {
-                this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Button;component/assets/Japan.png")));
+                // see if Carlos has already eaten the curry
+                if (curryEaten)
+                {
+                    this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Button;component/assets/Japan empty curry.png")));
+                }
+                else
+                {
+                    this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Button;component/assets/Japan.png")));
+                }
+
                 btnCarlos.Source = new BitmapImage(new Uri(@"/assets/Carlos button.png", UriKind.Relative));
             }
             risingSun.Stop();
-            sleepTimer.Stop();
             Point mousePos = e.GetPosition(this);
 
             // check x, y coordinates to see if they match the x, y coordinates of the elements on the map
@@ -105,15 +127,27 @@ namespace Button
             {
                 // Carlos is driving through Mount Haruna
                 btnCarlos.Source = new BitmapImage(new Uri(@"/assets/drive.png", UriKind.Relative));
-                drivingSound.Play();
-                Storyboard driveSB = this.FindResource("DriveInitialD") as Storyboard;
+
+
+
+
+                Storyboard driveSB = this.Resources["DriveInitialD"] as Storyboard;
                 driveSB.Begin();
+
+
+
+
+
+
+                drivingSound.Play();
                 pressed = false;
                 Debug.WriteLine("Vroom vroom");
             } else if (((mousePos.X >= 270) & (mousePos.X <= 270 + btnCarlos.Width)) & ((mousePos.Y >= 660) & (mousePos.Y <= 660 + btnCarlos.Height)))
             {
                 // Carlos eats curry
                 btnCarlos.Source = new BitmapImage(new Uri(@"/assets/Carlos eating button.png", UriKind.Relative));
+                curryEaten = true;
+                this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Button;component/assets/Japan empty curry.png")));
                 Debug.WriteLine("Curry");
             } else if (((mousePos.X >= 495) & (mousePos.X <= 495 + btnCarlos.Width)) & ((mousePos.Y >= 485) & (mousePos.Y <= 485 + btnCarlos.Height)))
             {
@@ -126,25 +160,21 @@ namespace Button
                 btnCarlos.Source = new BitmapImage(new Uri(@"/assets/Carlos holding coffee.png", UriKind.Relative));
                 Debug.WriteLine("Vending machine");
             }
-
         }
 
 
         private void btnCarlos_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            sleepTimer.Start();
             pressed = false;
         }
 
-        // Utility function
-        private void Timer_Tick(object sender, EventArgs e)
+        // The code below will run if the right button is clicked
+        private void btnCarlos_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // currently background will not updated and btnCarlos will not update
             // Carlos' fondest memory is night time in Japan
             btnCarlos.Source = new BitmapImage(new Uri(@"/assets/Carlos starry eyed.png", UriKind.Relative));
             this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Button;component/assets/Tokyo night.jpg")));
             tokyoBackground = true;
-            sleepTimer.Stop();
             risingSun.Play();
             Debug.Write("Tokyo night!");
         }
